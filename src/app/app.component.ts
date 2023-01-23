e20cfb48f2f5dfd07dcb22cfb3bbc9bb09610b3d
duqkegol1 <duqkegol1@gmail.com>
2023-01-23 10:10:31 -0500

    pc --devDQ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { compileNgModule } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,32 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 })
 
 export class AppComponent {
-  
+
   constructor(private http: HttpClient) {
   }
   ngOnInit(): void {
-      this.datos.subscribe(x =>{
-        console.log(x)
-      })
+    // this.datos.subscribe(x =>{
+    //   console.log(x)
+    // })
+  }
+  images = ['assets/17010.jpg', 'assets/dk.jpg', 'assets/Oliver_Atom_Adolescente.png'];
+  imagen_carga = '';
+  lol(event: any) {
+    let img_cont = document.getElementById('img-cont');
+    let imgUrl = event.target.dataset.imgurl;
+    this.imagen_carga = imgUrl;
+    // console.log(imgUrl);
   }
   
-  title = 'angular_testing';
-  miPropiedad = 'Hola';
-  miArreglo: any = [1,7, 9];
+  lol2(event: any) {
+    
+    console.log(event.target.src);
+  }
+  
+  // name! : string;
+  // title = 'angular_testing';
+  // miPropiedad = 'Hola';
+  // miArreglo: any = [1,7, 9];
 
-  datos = this.http.get('http://app.digitalgene.com.devel/get-tableros-prueba');
+  // datos = this.http.get('http://app.digitalgene.com.devel/get-tableros-prueba');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JosueComponent } from './josue/josue.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { JosueComponent } from './josue/josue.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
